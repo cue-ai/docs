8b0c9420e643c399b20acbb3a4e2162f16441b8d
Caelin Sutch <caelinsutch@gmail.com>
2023-12-09 13:51:59 -0800

    Docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import {useRouter} from "next/router";
 
 const config: DocsThemeConfig = {
   logo: <span>Cue AI</span>,
@@ -10,6 +11,15 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'Cue AI',
   },
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        description: 'Cue AI Documentation',
+        titleTemplate: '%s – Cue AI'
+      }
+    }
+  }
 }
 
 export default config

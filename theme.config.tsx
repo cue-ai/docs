a870a036b819e4652b14c987871c8721a3b178be
Caelin Sutch <caelinsutch@gmail.com>
2023-12-09 12:43:43 -0800

    Oh yah

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Cue AI</span>,
+  project: {
+    link: 'https://github.com/cue-ai/docs',
+  },
+  docsRepositoryBase: 'https://github.com/cue-ai/docs',
+  footer: {
+    text: 'Cue AI',
+  },
+}
+
+export default config
